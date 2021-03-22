a3a2974cba360daea0b63c2fa0a4a8d2e749ba0a
Joshua Arroyo <Jalexarroyo@gmail.com>
2021-03-22 00:00:01 -0700

    put route created and working

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -5,8 +5,8 @@ const workoutSchema = new Schema({
     type: Date,
   },
   exercises: {
-    type: { type: String },
-    name: { type: String },
+    type: { type: String, },
+    name: { type: String, },
     duration: {
         type: Number
     },
@@ -19,7 +19,20 @@ const workoutSchema = new Schema({
     sets: {
         type: Number
     },
+    distance: {
+      type: Number,
+    }
   },
-});
+},
+// {
+//   toJSON: {
+//     virtuals: true,
+//   },
+// }
+);
+
+// workoutSchema.virtual("totalDuration").get(() => {
+//   return this.exercises.reduce((total, exercise) => total + exercise, 0);
+// });
 const Workout = mongoose.model("Workout", workoutSchema);
 module.exports = Workout;
--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -13,8 +13,11 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
-router.put("/api/workouts:id", (req, res) => {
-    workout.update({_id: db.ObjectId(req.params.id)}, {$set: {"type": req.body.type, "name": req.body.name, "duration": req.body.duration, "weight": req.body.weight, "reps": req.body.reps, "sets": req.body.sets}})
+router.put("/api/workouts/:id", (req, res) => {
+    workout.findByIdAndUpdate(req.params.id,
+      {$push:{exercises: req.body}},
+      {new: true}
+    )
       .then(dbworkout => {
           console.log(dbworkout);
         res.json(dbworkout);
@@ -23,18 +26,9 @@ router.put("/api/workouts:id", (req, res) => {
         res.status(400).json(err);
       });
   });
-// router.post("/api/transaction/bulk", ({ body }, res) => {
-//   Transaction.insertMany(body)
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
 
 router.get("/api/workouts", (req, res) => {
-  workout.find({})
+  workout.find()
     .sort({ date: -1 })
     .then(dbTransaction => {
       res.json(dbTransaction);
@@ -42,6 +36,17 @@ router.get("/api/workouts", (req, res) => {
     .catch(err => {
       res.status(400).json(err);
     });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  workout.find()
+  .sort({ date: -1 })
+  .then(dbTransaction => {
+    res.json(dbTransaction);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
 });
 
 module.exports = router;
